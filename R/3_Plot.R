de9bf6d22951e68d99b94ceac48385ee76bf7fcb
linnvassvik <linnvassvik@gmail.com>
2025-06-06 12:40:35 +0200

    added plots on general data

--- R/3_Plot.R
+++ R/3_Plot.R
@@ -233,6 +233,52 @@ SpeciesVisit <- Visits4_summarised %>%
 ggsave(SpeciesVisit, filename = "Figures/SpeciesVisit.jpeg", height = 8, width = 13)
 
 
+GenusVisit <- Visits3 %>% 
+  ggplot(aes(y = Count, x = Pollinator, fill = Type)) + 
+  geom_col(position = position_dodge()) +
+  scale_fill_manual(name = "",
+                    values = c("Tree" = "#7F646C", "Ground" = "#CC9966"),
+                    labels = c("Tree" = "Apple flowers", "Ground" = "Dandelions")) +
+  theme_minimal() +
+  labs(title = NULL,
+       x = "",
+       y = "Total number") +
+  ggtitle("") + 
+  theme(plot.title = element_text(hjust = 1),
+        axis.text.x = element_text(angle = 45, hjust = 1, size = 16, face = "italic"),
+        axis.title.y = element_text(size = 15),
+        legend.text = element_text(size = 16))
+
+ggsave(GenusVisit, filename = "Figures/GenusVisit.jpeg", height = 8, width = 13)
+
+
+# Plot from model output
+emm <- emmeans(ManualObsModel2b, ~ Where * Pollinator)
+
+
+emm_df <- summary(emm, type = "response")
+
+
+GenusModel2 <- ggplot(emm_df, aes(x = Pollinator, y = response, fill = Where)) +
+  geom_col(position = position_dodge(width = 0.8)) +
+  geom_errorbar(aes(ymin = asymp.LCL, ymax = asymp.UCL),
+                position = position_dodge(width = 0.8), width = 0.2) +
+  labs(
+    y = "Estimated number of visits per flower",
+    x = "",
+    fill = "Pollinator"
+  ) +
+  scale_fill_manual(name = "",
+                    values = c("Tree" = "#7F646C", "Ground" = "#CC9966"),
+                    labels = c("Tree" = "Apple flowers", "Ground" = "Dandelions")) +
+  theme_minimal() +
+  theme(plot.title = element_text(hjust = 1),
+        axis.text.x = element_text(angle = 45, hjust = 1, size = 16),
+        axis.title.y = element_text(size = 15),
+        legend.text = element_text(size = 16))
+
+
+ggsave(GenusModel2, filename = "Figures/GenusModel2.jpeg", height = 8, width = 13)
 
 # Q3: What effect does mowing have on pollinator visitations? -------------
 
@@ -313,3 +359,199 @@ ggsave("Figures/Pheno_mowing.png", plot = Pheno_mowing, width = 10, height = 8, 
 MowingTreatment <- ggarrange(MowingProject, Pheno_mowing, common.legend = TRUE, legend = "bottom", labels = c("a", "b"))
 ggsave("Figures/MowingTreatment.png", plot = MowingTreatment, width = 10, height = 6, dpi = 300)
 
+
+
+# Extra -------------------------------------------------------------------
+
+FlowersLocation <- ManualVis_per_flower %>% 
+  ggplot(aes(x = Where, y = NOpen, color = Where, fill = Where)) +
+  geom_col() +
+  scale_color_manual(name = "",
+                     values = c("Tree" = "#7F646C", "Ground" = "#CC9966"),
+                     labels = c("Tree" = "Apple flowers", "Ground" = "Dandelions")) +
+  scale_fill_manual(name = "",
+                    values = c("Tree" = "#7F646C", "Ground" = "#CC9966"),
+                    labels = c("Tree" = "Apple flowers", "Ground" = "Dandelions")) +
+  theme_minimal() +
+  labs(x = "", y = "Number of open flowers") +
+  theme(axis.text.x = element_blank(),
+        strip.text = element_text(face = "bold")) +
+  facet_wrap(~ Location,
+             strip.position = "bottom",
+             switch = "x")
+
+
+ggsave("Figures/FlowersLocation.png", plot = FlowersLocation, width = 10, height = 6, dpi = 300)
+
+
+Visits_per_flower %>% 
+  mutate(Apple_variety = fct_recode(Apple_variety,
+                                    "Mowing" = "Discovery",
+                                    "No mowing" = "Summerred")) %>%
+  ggplot(aes(x = Apple_variety, y = Total_Open, color = Apple_variety, fill = Apple_variety)) +
+  geom_col() +
+  scale_fill_manual(values = c("Mowing" = "#669999", "No mowing" = "#7F646C")) +
+  scale_color_manual(values = c("Mowing" = "#669999", "No mowing" = "#7F646C")) +
+  theme_minimal() +
+  labs(x = "", y = "Number of open flowers") +
+  theme(axis.text.x = element_blank(),
+        strip.text = element_text(face = "bold")) +
+  facet_wrap(~ Location,
+             strip.position = "bottom",
+             switch = "x")
+
+
+
+
+
+# Other -------------------------------------------------------------------
+#NOT USED
+
+
+# Visits3_other %>% 
+#   ggplot(aes(x = ))
+# 
+# 
+# #DOY
+# 
+# Visits24_DOY %>% 
+#   ggplot(aes(x = DOY, y = n, fill = Taxanomic_group, color = Taxanomic_group)) +
+#   geom_smooth() +
+#   labs(x = "Day of year (DOY)", y = "Number of pollinators active", fill = "", color = "") +
+#   scale_fill_manual(values = c("#FFAC81", "#B74F6F", "#FEC3A6")) +
+#   scale_color_manual(values = c("#FFAC81","#B74F6F", "#FEC3A6")) +
+#   scale_x_continuous(breaks = sort(unique(Visits24_DOY$DOY))) +
+#   theme_minimal()
+#   
+#   
+# # 1. Create new data frame for prediction: all DOY values and all Taxanomic_groups
+# newdata <- expand.grid(
+#   DOY = seq(min(Visits24_DOY$DOY), max(Visits24_DOY$DOY), by = 1),
+#   Taxanomic_group = unique(Visits24_DOY$Taxanomic_group),
+#   Location = NA   # random effect set to zero
+# )
+# 
+# # 2. Predict on newdata (type = "response" to get counts), with standard errors
+# preds <- predict(VisitsDOY2, newdata = newdata, type = "response", se.fit = TRUE)
+# 
+# newdata <- newdata %>%
+#   mutate(
+#     fit = preds$fit,
+#     se = preds$se.fit,
+#     lower = fit - 1.96 * se,
+#     upper = fit + 1.96 * se
+#   )
+# 
+# # 3. Plot model predictions with ribbons and lines, using your color scheme
+# DOYActivity <- ggplot(newdata, aes(x = DOY, y = fit, color = Taxanomic_group, fill = Taxanomic_group)) +
+#   geom_ribbon(aes(ymin = lower, ymax = upper), alpha = 0.2, color = NA) +
+#   geom_line(size = 1) +
+#   labs(x = "Day of year (DOY)", y = "", fill = "", color = "") +
+#   scale_fill_manual(values = c("#B74F6F", "#FFAC81", "#FEC3A6")) +
+#   scale_color_manual(values = c("#B74F6F", "#FFAC81", "#FEC3A6")) +
+#   scale_x_continuous(breaks = sort(unique(Visits24_DOY$DOY))) +
+#   theme_minimal()
+# 
+# ggsave("Figures/DOYActivity.png", plot = DOYActivity, width = 10, height = 8, dpi = 300)
+# 
+# ## Time
+# 
+#   
+# Visits24_Time %>%
+#   mutate(Hour = hour(hm(Time_rounded))) %>%
+#   ggplot(aes(x = Hour, y = n, color = Taxanomic_group, fill = Taxanomic_group)) +
+#   geom_smooth() +
+#   scale_x_continuous(
+#     breaks = seq(0, 23, by = 2),
+#     labels = sprintf("%02d:00", seq(0, 23, by = 2))) +
+#   labs(x = "Hour of day", y = "Number of pollinators active", fill = "", color = "") +
+#   scale_fill_manual(values = c("#FFAC81", "#B74F6F", "#FEC3A6")) +
+#   scale_color_manual(values = c("#FFAC81","#B74F6F", "#FEC3A6")) +
+#   theme_minimal()
+# 
+# 
+# 
+# 
+# 
+# # 1. Create new data frame for prediction: all DOY values and all Taxanomic_groups
+# newdata <- expand.grid(
+#   Hour = seq(min(Visits24_Time$Hour), max(Visits24_Time$Hour), by = 1),
+#   Taxanomic_group = unique(Visits24_Time$Taxanomic_group),
+#   Location = NA   # random effect set to zero
+# )
+# 
+# # 2. Predict on newdata (type = "response" to get counts), with standard errors
+# preds <- predict(VisitsTime, newdata = newdata, type = "response", se.fit = TRUE)
+# 
+# newdata <- newdata %>%
+#   mutate(
+#     fit = preds$fit,
+#     se = preds$se.fit,
+#     lower = fit - 1.96 * se,
+#     upper = fit + 1.96 * se
+#   )
+# 
+# # 3. Plot model predictions with ribbons and lines, using your color scheme
+# TimeActivity <- ggplot(newdata, aes(x = Hour, y = fit, color = Taxanomic_group, fill = Taxanomic_group)) +
+#   geom_ribbon(aes(ymin = lower, ymax = upper), alpha = 0.2, color = NA) +
+#   geom_line(size = 1) +
+#   labs(x = "Time of day", y = "Predicted number of pollinators active", fill = "", color = "") +
+#   scale_fill_manual(values = c("#FFAC81", "#B74F6F", "#FEC3A6")) +
+#   scale_color_manual(values = c("#FFAC81","#B74F6F", "#FEC3A6")) +
+#   scale_x_continuous(breaks = sort(unique(Visits24_Time$Hour))) +
+#   theme_minimal()
+# 
+# 
+# ggsave("Figures/TimeActivity.png", plot = TimeActivity, width = 10, height = 8, dpi = 300)
+# 
+# 
+# 
+# 
+# 
+# Visits24_Temp %>% 
+#   ggplot(aes(x = Temperature, y = n, fill = Taxanomic_group, color = Taxanomic_group))+
+#   geom_smooth() +
+#   labs(x = "Temperature (Celsius)", y = "Number of pollinators active", fill = "", color = "") +
+#   scale_fill_manual(values = c("#FFAC81", "#B74F6F", "#FEC3A6")) +
+#   scale_color_manual(values = c("#FFAC81","#B74F6F", "#FEC3A6")) +
+#   scale_x_continuous(breaks = seq(floor(min(Visits24_Temp$Temperature, na.rm = TRUE)),
+#                  ceiling(max(Visits24_Temp$Temperature, na.rm = TRUE)), by = 3)) +
+#   theme_minimal()
+# 
+# 
+# 
+# 
+# # 1. Create new data frame for prediction: all DOY values and all Taxanomic_groups
+# newdata <- expand.grid(
+#   Temperature = seq(min(Visits24_Temp$Temperature), max(Visits24_Temp$Temperature), by = 1),
+#   Taxanomic_group = unique(Visits24_Temp$Taxanomic_group),
+#   Location = NA   # random effect set to zero
+# )
+# 
+# # 2. Predict on newdata (type = "response" to get counts), with standard errors
+# preds <- predict(VisitsTemp, newdata = newdata, type = "response", se.fit = TRUE)
+# 
+# newdata <- newdata %>%
+#   mutate(
+#     fit = preds$fit,
+#     se = preds$se.fit,
+#     lower = fit - 1.96 * se,
+#     upper = fit + 1.96 * se
+#   )
+# 
+# # 3. Plot model predictions with ribbons and lines, using your color scheme
+# TempActivity <- ggplot(newdata, aes(x = Temperature, y = fit, color = Taxanomic_group, fill = Taxanomic_group)) +
+#   geom_ribbon(aes(ymin = lower, ymax = upper), alpha = 0.2, color = NA) +
+#   geom_line(size = 1) +
+#   labs(x = "Temperature (Celsius)", y = "", fill = "", color = "") +
+#   scale_fill_manual(values = c("#B74F6F", "#FFAC81", "#FEC3A6")) +
+#   scale_color_manual(values = c("#B74F6F", "#FFAC81", "#FEC3A6")) +
+#   scale_x_continuous(breaks = sort(unique(Visits24_Temp$Temperature))) +
+#   theme_minimal()
+# 
+# ggsave("Figures/TempActivity.png", plot = TempActivity, width = 10, height = 8, dpi = 300)
+# 
+# 
+# Activity_Camera <- ggarrange(DOYActivity, TimeActivity, TempActivity, nrow = 3, common.legend = TRUE, legend = "bottom", labels = c("a", "b", "C"), label.y = 1.07, label.x = 0.02)
+# 
+# ggsave("Figures/Activity_Camera.png", plot = Activity_Camera, width = 10, height = 10, dpi = 300)
